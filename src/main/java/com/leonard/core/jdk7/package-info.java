de05452781765228b7cf35c5eb808392ba7bcef7
Leonard <941668690@qq.com>
2016-07-18 23:26:47 +0800

    init commit

---
+++ src/main/java/com/leonard/core/jdk7/package-info.java
@@ -0,0 +1,5 @@
+/**
+ *  JDK 7 新特性
+ * Created by Leonard on 2016/7/18.
+ */
+package com.leonard.core.jdk7;
---
+++ src/main/java/com/leonard/core/jdk8/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * JDK 8 新特性
+ * Created by Leonard on 2016/7/18.
+ */
+package com.leonard.core.jdk8;
